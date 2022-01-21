34756128ab65a4d0b83d12bc1d14d95d1f8094df
Ebraj Grg <gurungebraj22@gmail.com>
2022-01-21 09:55:47 +0545

    initial-commit

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="bg-white border-t">
+      <div className="container-1200 px-5 py-5">
+        <p className="text-center">
+          © Copyight 2022. Ebraj - All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
